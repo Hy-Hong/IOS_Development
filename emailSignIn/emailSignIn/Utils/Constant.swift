c1cd8da2d0c3fb3acb7cdebfa9853b087cb80f9d
Hy Horng <hy.hong2020@gmail.com>
2020-12-18 11:01:36 +0700

    my email sign in

---
+++ emailSignIn/emailSignIn/Utils/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  emailSignIn
+//
+//  Created by Hy Horng on 11/13/20.
+//  Copyright © 2020 Hy Horng. All rights reserved.
+//
+
+import UIKit
+
+let GREEN_THEME = UIColor(displayP3Red: 109 / 255, green: 201 / 255, blue: 149 / 255, alpha: 1)
